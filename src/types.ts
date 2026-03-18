e629edfa699d0b94fb68b25bfd14f67a9ac0aa37
Danrley3110 <danrleyfelipe58@gmail.com>
2026-03-18 19:46:21 -0300

    Atualização src

---
+++ src/types.ts
@@ -0,0 +1,39 @@
+export interface Product {
+  id: string;
+  name: string;
+  description: string;
+  category: string;
+  image: string;
+  originalPrice: number;
+  promoPrice: number;
+  discount: number;
+  affiliateLink: string;
+  clicks: number;
+  isPopular?: boolean;
+}
+
+export interface Banner {
+  id: string;
+  image: string;
+  title: string;
+  subtitle: string;
+  link: string;
+}
+
+export interface VideoStory {
+  id: string;
+  videoUrl: string;
+  productName: string;
+  affiliateLink: string;
+}
+
+export interface SiteSettings {
+  primaryColor: string;
+  secondaryColor: string;
+  buttonColor: string;
+  fontFamily: string;
+  instagram: string;
+  facebook: string;
+  whatsapp: string;
+  adminPassword?: string;
+}
